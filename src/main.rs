1dedb9a21825cffdaee4e546f2d3a5ad814f164d
sarmat <skhachirov12@gmail.com>
2023-10-07 14:34:56 +0300

    feat: save subscribers & tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-  HttpResponse::Ok()
-}
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-    .bind("127.0.0.1:8000")?
-    .run()
+  let configuration = get_configuration().expect("Failed to read configuration.");
+  let connection_pool = PgPool::connect(&configuration.database.connection_string())
     .await
+    .expect("Failed to connect to Postgres.");
+
+  let address = format!("127.0.0.1:{}", configuration.application_port);
+  let listener = TcpListener::bind(address)?;
+  run(listener, connection_pool)?.await?;
+  Ok(())
 }
